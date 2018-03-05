45687b01dc6f2ed22ffa88df68899146173c3400
Marcos Rafael <marcosrrj2@gmail.com>
2018-03-05 10:21:27 -0300

    Personalizando o Retorno da Validação

---
+++ src/main/java/com/cursospring/ionic/cursosi/controller/exception/FieldMessage.java
@@ -0,0 +1,36 @@
+package com.cursospring.ionic.cursosi.controller.exception;
+
+import java.io.Serializable;
+
+public class FieldMessage implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private String fieldName;
+	private String msg;
+
+	public FieldMessage() {
+
+	}
+	public FieldMessage(String fieldName, String msg) {
+		this.fieldName = fieldName;
+		this.msg = msg;
+	}
+
+	public String getFieldName() {
+		return fieldName;
+	}
+
+	public void setFieldName(String fieldName) {
+		this.fieldName = fieldName;
+	}
+
+	public String getMsg() {
+		return msg;
+	}
+
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+
+}
