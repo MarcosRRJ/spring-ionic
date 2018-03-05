d83d10b8d956f94f69b464a0c62182a6f67a1083
Marcos Rafael <marcosrrj2@gmail.com>
2018-03-05 12:32:41 -0300

    Validação Customizada: email não pode ser repitido na inserção

--- src/main/java/com/cursospring/ionic/cursosi/repository/ClienteRepository.java
+++ src/main/java/com/cursospring/ionic/cursosi/repository/ClienteRepository.java
@@ -1,11 +1,16 @@
 package com.cursospring.ionic.cursosi.repository;
 
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.cursospring.ionic.cursosi.model.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer>{
 
+	@Transactional(readOnly=true)
+	public Cliente findByEmail(String email);
+	
 }
