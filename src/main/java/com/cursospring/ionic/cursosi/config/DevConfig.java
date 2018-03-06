9eb3cd6e298f6b13f06a8a0a1bb39a0429e87137
Marcos Rafael <marcosrrj2@gmail.com>
2018-03-06 14:03:27 -0300

    Implementando SmtpEmailService com servidor do Google

--- src/main/java/com/cursospring/ionic/cursosi/config/DevConfig.java
+++ src/main/java/com/cursospring/ionic/cursosi/config/DevConfig.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.cursospring.ionic.cursosi.service.DBService;
+import com.cursospring.ionic.cursosi.service.EmailService;
+import com.cursospring.ionic.cursosi.service.SmtpEmailService;
 
 @Configuration
 @Profile("dev")
@@ -30,5 +32,12 @@ public class DevConfig {
 		dbService.instantiateTestDatabase();
 
 		return true;
+	}
+
+	@Bean
+	public EmailService emailService() {
+
+		return new SmtpEmailService();
+
 	}
 }
---
+++ src/main/java/com/cursospring/ionic/cursosi/service/SmtpEmailService.java
@@ -0,0 +1,26 @@
+package com.cursospring.ionic.cursosi.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
+
+import com.cursospring.ionic.cursosi.serviceimpl.AbstractEmailService;
+
+public class SmtpEmailService extends AbstractEmailService {
+
+	@Autowired
+	private MailSender mailSender;
+
+	private static final Logger LOG = LoggerFactory.getLogger(SmtpEmailService.class);
+
+	@Override
+	public void sendEmail(SimpleMailMessage msg) {
+
+		LOG.info("Enviando envio de email...");
+		mailSender.send(msg);
+		LOG.info("Email enviado");
+	}
+
+}
